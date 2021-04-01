15467842dae6d507c1d0579c4c921cd3d165c176
sflak <sflak@splunk.com>
2021-04-01 12:18:19 -0700

    Connect to mongoDB

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,25 @@
-import * as express from 'express';
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import apiRouter from './routes';
 
 const app = express();
 
 app.use(express.static('public'));
+
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb" }));
+app.use(cors());
+
 app.use(apiRouter);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
+// TODO: use env variables for db creds
+const CONNECTION_URL = 'mongodb+srv://sflak_proj:n2S1EKjA4BPp5xNg@cluster0.6lbxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
